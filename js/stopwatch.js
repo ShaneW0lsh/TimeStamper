31c0cb08c313ef72773dd5998c84b79520552214
Daniil Kolomychenko <shanebetterfather@gmail.com>
2024-10-04 03:14:08 +0300

    Add good timer

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,15 +1,31 @@
 let startTime, endTime, running, duration = 0;
+let timerInterval;
 
 function startTimer() {
   if (running) return;
   running = true;
+  disableTrashAnimation();
   startTime = performance.now();
 }
 
 
 function stopTimer() {
+  running = false;
   endTime = performance.now();
   duration += (endTime - startTime) / 1000;
+  enableTrashAnimation();
+}
+
+function resetTimer() {
+  if (running) return;
+  startTime = null;
+  endTime = null;
+  running = false;
+  duration = 0;
+  updateDisplay(0);
+  document.getElementById('start-button').disabled = false;
+  document.getElementById('stop-button').disabled = true;
+  disableTrashAnimation();
 }
 
 
@@ -17,14 +33,50 @@ function updateDisplay(time) {
   const hours = Math.floor(time / 3600);
   const minutes = Math.floor((time % 3600) / 60);
   const seconds = Math.floor(time % 60);
-  const milliseconds = Math.floor((time % 1) * 1000);
 
-  const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(3, '0')}`;
-  document.getElementById('timer').textContent = formattedTime;
+  // const milliseconds = Math.floor((time % 1) * 100);
+  // document.getElementById('timerDisplay').textContent =
+  //   `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(2, '0')}`;
+
+  document.getElementById('timerDisplay').textContent =
+    `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
-document.getElementById('start-button').addEventListener('click', startTimer);
-document.getElementById('stop-button').addEventListener('click', stopTimer);
+window.addEventListener('load', function() {
+  disableTrashAnimation();
+})
+
+function disableTrashAnimation() {
+  const stopwatchContainer = document.getElementById('timerContainer');
+  stopwatchContainer.classList.remove('stopwatch-container');
+}
+
+function enableTrashAnimation() {
+  const stopwatchContainer = document.getElementById('timerContainer');
+  stopwatchContainer.classList.add('stopwatch-container');
+}
+
+document.getElementById('start-button').addEventListener('click', function () {
+  startTimer();
+  this.disabled = true;
+  document.getElementById('stop-button').disabled = false;
+  // document.getElementById('resetBtn').disabled = true;
+  timerInterval = setInterval(function () {
+    updateDisplay(duration + (performance.now() - startTime) / 1000);
+  }, 10);
+});
+
+document.getElementById('stop-button').addEventListener('click', function () {
+  stopTimer();
+  this.disabled = true;
+  document.getElementById('start-button').disabled = false;
+  // document.getElementById('resetBtn').disabled = false;
+  clearInterval(timerInterval);
+});
+
+document.getElementById('timerContainer').addEventListener('click', function () {
+  resetTimer();
+})
 
 // document.addEventListener('keydown', function (event) {
 //   switch (event.key) {
